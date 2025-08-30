104ed44bd02e9e94ead416746da56b425c2400bd
harshvardhan-1210 <harshatadimarri@gmail.com>
2025-08-30 10:59:13 +0530

    commited root modules

---
+++ Terrafrom/outputs.tf
@@ -0,0 +1,8 @@
+output "vpc_id" { value = module.vpc.vpc_id }
+output "public_subnets" { value = module.vpc.public_subnets }
+output "private_subnets" { value = module.vpc.private_subnets }
+output "eks_cluster_id" { value = module.eks.cluster_id }
+output "eks_cluster_endpoint" { value = module.eks.cluster_endpoint }
+output "eks_cluster_ca" { value = module.eks.cluster_certificate_authority_data }
+output "s3_bucket_id" { value = module.s3.bucket_id }
+output "s3_bucket_arn" { value = module.s3.bucket_arn }
---
+++ Terrafrom/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" { default = "us-east-1" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "public_subnets" { default = ["10.0.1.0/24", "10.0.2.0/24"] }
+variable "private_subnets" { default = ["10.0.3.0/24", "10.0.4.0/24"] }
+variable "availability_zones" { default = ["us-east-1a", "us-east-1b"] }
+variable "cluster_name" { default = "my-eks-cluster" }
+variable "cluster_version" { default = "1.29" }
+variable "bucket_name" { default = "my-eks-flask-bucket" }
+variable "tags" {
+  default = {
+    Project = "Flask-Postgres-EKS"
+    Owner   = "Harsha"
+  }
+}
